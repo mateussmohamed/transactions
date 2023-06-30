699a40eb121315672c1200c25e2ba87a295861db
Mateus Santana <mateuss.dev@gmail.com>
2023-06-30 18:03:25 -0300

    fix: solving drizzle config error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './database/schema.ts',
   out: './database/migrations',
-  connectionString: process.env.POSTGRES_URL
+  dbCredentials: {
+    connectionString: String(process.env.POSTGRES_URL)
+  }
 } satisfies Config
