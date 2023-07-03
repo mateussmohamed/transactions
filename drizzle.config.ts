b1e4acb72ca4d0104756317769450615630c630a
Mateus Santana <mateuss.dev@gmail.com>
2023-07-03 11:58:02 -0300

    feat: config drizzle-orm studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './database/schema.ts',
   out: './database/migrations',
+  driver: 'pg',
   dbCredentials: {
     connectionString: String(process.env.POSTGRES_URL)
   }
